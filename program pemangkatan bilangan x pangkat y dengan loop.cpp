9f686acaa7bf96dd969189778e589929f3128285
nurasiyah <45659380+nurasiyah@users.noreply.github.com>
2019-01-05 15:16:56 +0700

    Add files via upload

---
+++ program pemangkatan bilangan x pangkat y dengan loop.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+    int main();
+    int X,Y,hasil;
+    printf("Masukkan nilai X =");scanf("%i",&X);
+    printf("Masukkan nilai Y =");scanf("%i",&Y);
+    printf("\n");
+    hasil:X;
+    for(int i=1;i<Y;i++){
+    printf("%i*%=%i\n",hasil,X,hasil*X);
+
+    }
+    printf("\nhasil:%i",hasil);
+    getch();
+}
